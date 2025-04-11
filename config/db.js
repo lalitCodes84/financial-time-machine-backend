938e3a8bd40895a52c2f46c2fc4c0e476acf9c39
lalitCodes84 <vickysharma71za@gmail.com>
2025-04-11 22:38:03 +0530

    🚀 fix: serverless MongoDB timeout using connection cache

--- config/db.js
+++ config/db.js
@@ -13,8 +13,6 @@ const connectDB = async () => {
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000, // important for serverless
     });
   }
